cf199e22fe0c24774576538167cc21d3e284af34
Vadim Chugunov <vadimcn@gmail.com>
2019-06-20 16:33:08 -0700

    Made native adapter the default, download platform files automatically.

--- extension/async.ts
+++ extension/async.ts
@@ -1,9 +1,29 @@
-import * as fs from 'fs';
-import * as cp from 'child_process';
+import * as _fs from 'fs';
+import * as _cp from 'child_process';
+import * as _http from 'http';
+import * as _https from 'https';
 import { promisify } from 'util';
 
-export const readdirAsync = promisify(fs.readdir);
-export const readFileAsync = promisify(fs.readFile);
-export const existsAsync = promisify(fs.exists);
-export const statAsync = promisify(fs.stat);
-export const execFileAsync = promisify(cp.execFile);
+export namespace fs {
+    export const readdir = promisify(_fs.readdir);
+    export const readFile = promisify(_fs.readFile);
+    export const exists = promisify(_fs.exists);
+    export const stat = promisify(_fs.stat);
+}
+
+export namespace cp {
+    export const execFile = promisify(_cp.execFile);
+}
+
+export namespace https {
+    export function get(options: _https.RequestOptions | string | URL): Promise<_http.IncomingMessage> {
+        return new Promise<_http.IncomingMessage>((resolve, reject) => {
+            try {
+                let request = _https.get(options, resolve);
+                request.on('error', err => reject(err));
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+}
