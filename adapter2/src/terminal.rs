31a1f5477807ab91dcc45be9bc209a6a156c8f33
Vadim Chugunov <vadimcn@gmail.com>
2019-06-26 18:56:03 -0700

    Terminal.

--- adapter2/src/terminal.rs
+++ adapter2/src/terminal.rs
@@ -1,13 +1,22 @@
+use crate::error::Error;
+use log::debug;
 use std::io::{self, BufRead};
 use std::net;
 
+#[cfg(windows)]
+extern "stdcall" {
+    fn FreeConsole() -> u32;
+    fn AttachConsole(pid: u32) -> u32;
+    fn GetLastError() -> u32;
+}
+
 pub struct Terminal {
     connection: net::TcpStream,
-    tty_name: String,
+    data: String,
 }
 
 impl Terminal {
-    pub fn create<F>(run_in_terminal: F) -> Result<Self, io::Error>
+    pub fn create<F>(run_in_terminal: F) -> Result<Self, Error>
     where
         F: FnOnce(Vec<String>),
     {
@@ -15,27 +24,63 @@ impl Terminal {
         let addr = listener.local_addr()?;
 
         // Opens TCP connection, send output of `tty`, wait till the socket gets closed from our end
-        let args = vec![
-            "/bin/bash".to_owned(),
-            "-c".to_owned(),
-            format!("exec 3<>/dev/tcp/127.0.0.1/{}; tty >&3; clear; read <&3", addr.port()),
-        ];
+        let mut executable = std::env::current_exe()?;
+
+        let args =
+            vec![executable.to_str().unwrap().into(), "terminal-agent".into(), format!("--port={}", addr.port())];
+
         run_in_terminal(args);
 
         let (stream, _) = listener.accept()?;
         let stream2 = stream.try_clone()?;
 
         let mut reader = io::BufReader::new(stream);
-        let mut tty_name = String::new();
-        reader.read_line(&mut tty_name)?;
+        let mut data = String::new();
+        reader.read_line(&mut data)?;
 
         Ok(Terminal {
             connection: stream2,
-            tty_name: tty_name.trim().to_owned(),
+            data: data.trim().to_owned(),
         })
     }
 
-    pub fn tty_name(&self) -> &str {
-        &self.tty_name
+    pub fn input_devname(&self) -> &str {
+        if cfg!(windows) {
+            "CONIN$"
+        } else {
+            &self.data
+        }
+    }
+
+    pub fn output_devname(&self) -> &str {
+        if cfg!(windows) {
+            "CONOUT$"
+        } else {
+            &self.data
+        }
+    }
+
+    pub fn attach<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce() -> R,
+    {
+        // Windows does not have an API for launching a child process attached to another console.
+        // Instead,
+        #[cfg(windows)]
+        {
+            let pid = self.data.parse::<u32>().unwrap();
+            unsafe {
+                dbg!(FreeConsole());
+                dbg!(AttachConsole(pid));
+            }
+            let result = f();
+            unsafe {
+                dbg!(FreeConsole());
+            }
+            result
+        }
+
+        #[cfg(not(windows))]
+        f()
     }
 }
